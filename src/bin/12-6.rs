9276d54f0b59ac00ba71bd3bd8b3236878b974a3
kpom <kpomeroy@mailgun.com>
2023-12-06 09:29:25 -0600

    This... just worked...

--- src/bin/12-6.rs
+++ src/bin/12-6.rs
@@ -1,4 +1,4 @@
-fn calculate_time(ms: i32, mm: i32) -> i32 {
+fn calculate_time(ms: i64, mm: i64) -> i64 {
     let mut ways_to_win = 0;
     for press in 0..ms {
         if (ms-press)*press > mm {
@@ -11,10 +11,12 @@ fn calculate_time(ms: i32, mm: i32) -> i32 {
 fn main() {
     // My data set (This was easier than parsing the file. Cheating? Maybe.)
     let records = [(38, 234), (67, 1027), (76, 1156), (73, 1236)];
+    
     let mut result = 1;
 
     for (ms, mm) in records {
         result *= calculate_time(ms, mm);
     }
-    println!("{}", result)
+    println!("{}", result);
+    println!("{}", calculate_time(38677673, 234102711561236));
 }
