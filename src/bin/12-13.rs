6e3a10f6c63b5a305ec1d77bbdb2a45e253dbcf1
kpom <kpomeroy@mailgun.com>
2023-12-13 14:28:59 -0600

    Clean those mirrors

--- src/bin/12-13.rs
+++ src/bin/12-13.rs
@@ -6,13 +6,17 @@ fn find_reflection(pattern: &Vec<String>) -> Option<usize> {
         // If this is the first or last line, we don't need to check 
         if index >= pattern.len() || index == 0 { continue }
 
+        let mut smudge_cleaned = false;
         // Starting from index, loop over every line in the pattern
         for (offset, i) in (index..pattern.len()).enumerate() {
 
             // The next comparison we would make would be negative. Since we reached
             // the end of the list without breaking, `index` is the reflection line.
             if offset >= index {
-                return Some(index);
+                if smudge_cleaned {
+                    return Some(index);
+                }
+                break
             }
 
             let pairing_line = index - offset - 1;
@@ -22,14 +26,26 @@ fn find_reflection(pattern: &Vec<String>) -> Option<usize> {
                 // If there are two lines...
                 (Some(a), Some(b)) => {
                     // ...and they don't match, we can bail completely
-                    if a != b { break }
+                    if a != b {
+                        if !smudge_cleaned && a.chars()
+                                            .zip(b.chars())
+                                            .filter(|(a, b)| a != b)
+                                            .count() == 1 {
+                            smudge_cleaned = true;
+                            println!("Cleaned smudge");
+                            println!("{a}");
+                            println!("{b}");
+                        } else {
+                            break 
+                        }
+                    }
                 }
                 // There shouldn't be a situation where this occurs due to careful checks above
                 _ => { return None }
             }
 
             // If this is the last line in the list, `index` is the reflection line
-            if i >= pattern.len()-1 {
+            if smudge_cleaned && i >= pattern.len()-1 {
                 return Some(index)
             }
         }
@@ -46,7 +62,7 @@ fn rotate_pattern(pattern: &Vec<String>) -> Vec<String>{
             if x >= new_pattern.len() {
                 new_pattern.push(String::new());
             }
-            &mut new_pattern[x].push_str(&ch.to_string());
+            let _ = &mut new_pattern[x].push_str(&ch.to_string());
         }
     }
 
